dcb91cc0108ca775220bab89f4ccd982212b8edc
Gustavo Pereira <gustavo.h.pereira13@aluno.senai.br>
2026-02-26 17:30:45 -0300

    aaaa

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import mysql.connector
-from model.musica import recuperar_musicas, enviar_musica
-from model.genero import recuperar_generos
+from model.musica import recuperar_musicas, enviar_musica, excluir_musica, ativo_inativo
+from model.genero import recuperar_generos, filtrar_generos
 
 app = Flask(__name__)
 
@@ -10,6 +10,14 @@ app = Flask(__name__)
 def pagina_index():
 
     musicas = recuperar_musicas()
+    genero = recuperar_generos()
+
+    return render_template("/principal.html", musicas = musicas, genero = genero)
+
+@app.route("/filtrar/<nome_genero>")
+
+def api_filtrar_musica(nome_genero):
+    musicas = filtrar_generos(nome_genero)
     genero = recuperar_generos()
 
     return render_template("/principal.html", musicas = musicas, genero = genero)
@@ -23,17 +31,34 @@ def pagina_adm():
 
     return render_template("/administracao.html", musicas = musicas, genero = genero)
 
-@app.route("/musica/post", methods = ["POST"])
+@app.route("/adm/post", methods = ["POST"])
 
 def api_inserir_musica():
     
     musica = request.form.get("musica")
     cantor = request.form.get("cantor")
     duracao = request.form.get("duracao")
+    atividade = "ATIVO"
     url_capa = request.form.get("url_capa")
     categoria = request.form.get("categoria")
     
-    if enviar_musica(cantor, duracao, musica, url_capa, categoria):
+    if enviar_musica(cantor, duracao, atividade, musica, url_capa, categoria):
+        return redirect("/adm")
+    else:
+        return "erro"
+    
+@app.route("/adm/delete/<int:codigo>")
+
+def api_deletar_musica(codigo):
+    if excluir_musica(codigo):
+        return redirect("/adm")
+    else:
+        return "erro"
+    
+@app.route("/adm/atividade/<int:codigo>/<atividade>")
+
+def api_atividade_musica(codigo, atividade):
+    if ativo_inativo(atividade, codigo):
         return redirect("/adm")
     else:
         return "erro"
--- model/musica.py
+++ model/musica.py
@@ -3,21 +3,20 @@ from database.conexao import conectar
 def recuperar_musicas():
     conexao, cursor = conectar()
 
-    cursor.execute("SELECT codigo, cantor, duracao, nome_musica, caminho_capa, nome_genero FROM musica")
+    cursor.execute("SELECT codigo, cantor, duracao, atividade, nome_musica, caminho_capa, nome_genero FROM musica ORDER BY atividade")
     musicas = cursor.fetchall()
 
     conexao.close()
 
     return musicas
 
-def enviar_musica(a:str,b:str,c:str,d:str,e:str) -> bool:
+def enviar_musica(a:str,b:str,c:str,d:str,e:str, f:str) -> bool:
     """Faz o envio dos dados formatados ao banco de dados e insere."""
-    conexao, cursor = conectar()
-
+    
     try:
-
-        cursor.execute("INSERT INTO musica(cantor, duracao, nome_musica, caminho_capa, nome_genero)" \
-        "VALUES(%s,%s,%s,%s,%s)", (a,b,c,d,e))
+        conexao, cursor = conectar()
+        cursor.execute("INSERT INTO musica(cantor, duracao, atividade, nome_musica, caminho_capa, nome_genero)" \
+        "VALUES(%s,%s,%s,%s,%s,%s)", (a,b,c,d,e,f))
 
         conexao.commit()
         conexao.close()
@@ -26,7 +25,39 @@ def enviar_musica(a:str,b:str,c:str,d:str,e:str) -> bool:
 
     except:
 
+        return False
+    
+def excluir_musica(a:int) -> bool:
+
+    try:
+        conexao, cursor = conectar()
+        cursor.execute("DELETE FROM musica " \
+               "WHERE codigo = %s", (a,))
+
+        conexao.commit()
         conexao.close()
+        
+        return True
+
+    except:
+
+        return False
+    
+def ativo_inativo(a:int, b:int) -> bool:
+
+    try:
+        conexao, cursor = conectar()
+
+        status_atual = "INATIVO" if a == "ATIVO" else "ATIVO"
+
+        cursor.execute("UPDATE musica SET atividade = %s WHERE codigo = %s", (status_atual,b))
+
+        conexao.commit()
+        conexao.close()
+        
+        return True
+    
+    except:
 
         return False
 
