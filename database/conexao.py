e5e06d623d3aabe4c3a74b21c413dbb6c3bb3b3b
Gustavo-Pereira20 <gustavo.h.pereira13@aluno.senai.br>
2026-03-27 15:58:46 -0300

    Add files via upload

--- database/conexao.py
+++ database/conexao.py
@@ -1,13 +1,21 @@
 import mysql.connector
 
 def conectar():
-            
-        conexao = mysql.connector.connect(
-            host = "localhost",
-            port = 3306,
-            user = "root",
-            password = "root",
-            database = "bcd_musica")
-
+        tipo = "NUVEM"
+        if tipo == "LOCAL":
+            conexao = mysql.connector.connect(
+                host = "localhost",
+                port = 3306,
+                user = "root",
+                password = "root",
+                database = "bcd_musica")
+        else:
+            conexao = mysql.connector.connect(
+                host = "servidor-tavao-servidor-tavao.a.aivencloud.com",
+                port = 26017,
+                user = "avnadmin",
+                password = "AVNS_TsFbdibzsDBGCyPHlxE",
+                database = "bcd_musica")
+                
         cursor = conexao.cursor(dictionary=True)
         return conexao, cursor
